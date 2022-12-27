7b5da8c7f4dd1525323771153734195f79d4debe
internMassiveStar <intern.massive@gmail.com>
2022-12-27 16:55:20 +0600

    section insert

--- src/app/user-pages/catelogue/createCategory.js
+++ src/app/user-pages/catelogue/createCategory.js
@@ -1,24 +1,48 @@
 import React, { Component, useEffect, useState } from "react";
-import './style.css';
-import Form from 'react-bootstrap/Form';
+import "./style.css";
+import Form from "react-bootstrap/Form";
+import axios from "axios";
+const baseUrl = "http://127.0.0.1:8000";
 
 function CreateCategory() {
-    return (
-        <div>
-            <div className="col-lg-12 grid-margin stretch-card">
-                <div className="card">
-                    <div className="card-body">
-                        <div className="btn-section">
-                            <h4 className="card-title">Create New Category</h4>
-                            <a className="btn-style btn btn-info" href="/catalogue/category"><i className="bi bi-list-columns-reverse"></i>All category</a>
-                        </div>
-                        <label className="label-style">Category name</label>
-                        <Form.Control className="col-sm-6" type="text" placeholder="Category name"></Form.Control>
-                        <a className="btn btn-success top-space"><i className="bi bi-save-fill"></i>Insert</a>
-                    </div>
-                </div>
+  const [name, setName] = useState();
+  const insert = () => {
+    axios
+      .post(`${baseUrl}/api/category-insert`, {
+        category_name: name,
+      })
+      .then((response) => {
+        alert(response.data.msg);
+      });
+  };
+  return (
+    <div>
+      <div className="col-lg-12 grid-margin stretch-card">
+        <div className="card">
+          <div className="card-body">
+            <div className="btn-section">
+              <h4 className="card-title">Create New Category</h4>
+              <a className="btn-style btn btn-info" href="/catalogue/category">
+                <i className="bi bi-list-columns-reverse"></i>All category
+              </a>
             </div>
+            <label className="label-style">Category name</label>
+            <Form.Control
+              className="col-sm-6"
+              type="text"
+              placeholder="Category name"
+              onChange={(event) => {
+                setName(event.target.value);
+                console.log(name);
+              }}
+            ></Form.Control>
+            <a className="btn btn-success top-space" onClick={insert}>
+              <i className="bi bi-save-fill"></i>Insert
+            </a>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 export default CreateCategory;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './app/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./app/App";
 import "./i18n";
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <BrowserRouter basename="/demo/corona-react-free/template/demo_1/preview">
+  <BrowserRouter>
     <App />
-  </BrowserRouter>
-, document.getElementById('root'));
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
