fb2d9f3e6fcf254412c296f3016e1b8e8caa7a1f
internMassiveStar <intern.massive@gmail.com>
2023-01-25 18:29:23 +0600

    commit from kalam

--- src/app/user-pages/recipe/newRecipe.js
+++ src/app/user-pages/recipe/newRecipe.js
@@ -1,76 +1,41 @@
 import React, { useEffect, useState } from "react";
-import './style.css';
+import "./style.css";
 import { Form } from "react-bootstrap";
 import { baseUrl, resturant_id } from "../constant/global";
 import axios from "axios";
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 
 function NewRecipe() {
-  const [foodName, setFoodName] = useState();
-  const [food, setFood] = useState();
-  const [branchId, setBranchId] = useState();
+  const [inputs, setInputs] = useState([{ value: "" }]);
 
-  useEffect(() => {
-    axios
-    .get(
-        `${baseUrl}/api/quick-foods/${resturant_id}/${
-        branchId ? branchId : resturant_id
-    }`
-    )
-    .then((response) => {
-        setFood(response.data);
-        console.log(response.data);
-    });
-  }, [])
-  
+  const handleInputChange = (index, event) => {
+    const values = [...inputs];
+    values[index].value = event.target.value;
+    setInputs(values);
+  };
+
+  const handleAddInput = () => {
+    setInputs([...inputs, { value: "" }]);
+  };
+
+  const handleRemoveInput = (index) => {
+    const values = [...inputs];
+    values.splice(index, 1);
+    setInputs(values);
+  };
   return (
     <div>
-      <div className="col-lg-12 grid-margin stretch-card">
-        <div className="card">
-          <div className="card-body">
-            <div className="btn-section">
-              <h4 className="card-title">Create New Recipe</h4>
-              <a className="btn-style btn btn-info" href="/waste/all"><i className="bi bi-card-list"></i> All
-                Recipe</a>
-            </div>
-            <div className="col-sm-12 background">
-              <div className='input_field two_part'>
-                <div className="wid">
-                  <Form.Label className="label-style">Food name</Form.Label>
-                  <select
-                    onChange={(event) => {
-                      setFoodName(event.target.value)
-                    }}
-                  >
-                    <option value="">Select food...</option>
-                    {
-                      food ?
-                        food.map((data) =>
-                          <option value={data.id}>
-                            {data.name}
-                          </option>
-                        ) : null
-                    }
-                  </select>
-                </div>
-              </div>
-              <div className="input_field">
-                <Form.Group>
-                  <Form.Label className="label-style">
-                    How To make step by step
-                  </Form.Label>
-                  <Form.Control
-                    className="area"
-                    as="textarea"
-                    placeholder="Meta keyword"
-                    rows={20}
-                  ></Form.Control>
-                </Form.Group>
-              </div>
-            </div>
-          </div>
+      {inputs.map((input, index) => (
+        <div key={index}>
+          <input
+            type="text"
+            value={input.value}
+            onChange={(event) => handleInputChange(index, event)}
+          />
+          <button onClick={() => handleRemoveInput(index)}>Remove</button>
         </div>
-      </div>
+      ))}
+      <button onClick={handleAddInput}>Add Input</button>
     </div>
   );
 }
