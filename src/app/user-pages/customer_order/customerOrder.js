68c1caa2c4ee4b5d4c33c5572449c2297462b2df
internMassiveStar <intern.massive@gmail.com>
2023-02-26 18:20:34 +0600

    commit from Kalam

--- src/app/user-pages/customer_order/customerOrder.js
+++ src/app/user-pages/customer_order/customerOrder.js
@@ -5,7 +5,7 @@ import "./style.css";
 import { baseUrl, restaurant_id, axios, Swal, Form } from "../constant/global";
 import ReactLoading from "react-loading";
 import Modal from "@mui/material/Modal";
-const customerId = sessionStorage.getItem("customer_id");
+const customerId = sessionStorage.getItem("emp_id");
 const token = sessionStorage.getItem("token");
 
 function CustomerOrder({}) {
@@ -166,7 +166,7 @@ function CustomerOrder({}) {
       .then((response) => {
         setDeliveryAddress(response.data);
       });
-  }
+  };
   const deliverDetails = (value) => {
     setPickUp(value);
     if (value == "home-delivery") {
@@ -191,15 +191,15 @@ function CustomerOrder({}) {
     axios.defaults.headers.common = { Authorization: `Bearer ${token}` };
     axios
       .post(`${baseUrl}/api/change-delivery-address/${customerId}`, {
-        'city': city,
-        'indication' : indication,
-        'address': address,
+        city: city,
+        indication: indication,
+        address: address,
       })
       .then((response) => {
         AddressModalClose();
         changeDeliverryAddress();
-      })
-  }
+      });
+  };
 
   return (
     <div>
@@ -244,55 +244,55 @@ function CustomerOrder({}) {
                     <tbody>
                       {orderDetails
                         ? orderDetails.map((data) => (
-                          <tr>
-                            <td>
-                              <img
-                                className="img-style"
-                                src={`${baseUrl}/foods/medium/${data[0].image}`}
-                              ></img>
-                            </td>
-                            <td>{data[0].food_name}</td>
-                            <td>{data[0].food_price}</td>
-                            <td>
-                              <button
-                                className="icon-plus"
-                                onClick={() => {
-                                  increaseQty(data[0].food_id);
-                                }}
-                              >
-                                <i className="bi bi-plus"></i>
-                              </button>
-                              {data[0].qty}
-                              <button
-                                className="icon-minus"
-                                onClick={() => {
-                                  descreaseQty(data[0].food_id);
-                                }}
-                              >
-                                <i className="bi bi-dash"></i>
-                              </button>
-                            </td>
-                            <td>{data[0].food_price * data[0].qty}</td>
-                            <td>
-                              {msp
-                                ? ((data[0].food_price * data[0].qty -
-                                  data[0].basic * data[0].qty) /
-                                  100) *
-                                msp
-                                : 0}
-                            </td>
-                            <td>
-                              <button
-                                className="icon-delete"
-                                onClick={() => {
-                                  removeItem(data[0].food_id);
-                                }}
-                              >
-                                <i className="bi bi-x-circle"></i>
-                              </button>
-                            </td>
-                          </tr>
-                        ))
+                            <tr>
+                              <td>
+                                <img
+                                  className="img-style"
+                                  src={`${baseUrl}/foods/medium/${data[0].image}`}
+                                ></img>
+                              </td>
+                              <td>{data[0].food_name}</td>
+                              <td>{data[0].food_price}</td>
+                              <td>
+                                <button
+                                  className="icon-plus"
+                                  onClick={() => {
+                                    increaseQty(data[0].food_id);
+                                  }}
+                                >
+                                  <i className="bi bi-plus"></i>
+                                </button>
+                                {data[0].qty}
+                                <button
+                                  className="icon-minus"
+                                  onClick={() => {
+                                    descreaseQty(data[0].food_id);
+                                  }}
+                                >
+                                  <i className="bi bi-dash"></i>
+                                </button>
+                              </td>
+                              <td>{data[0].food_price * data[0].qty}</td>
+                              <td>
+                                {msp
+                                  ? ((data[0].food_price * data[0].qty -
+                                      data[0].basic * data[0].qty) /
+                                      100) *
+                                    msp
+                                  : 0}
+                              </td>
+                              <td>
+                                <button
+                                  className="icon-delete"
+                                  onClick={() => {
+                                    removeItem(data[0].food_id);
+                                  }}
+                                >
+                                  <i className="bi bi-x-circle"></i>
+                                </button>
+                              </td>
+                            </tr>
+                          ))
                         : null}
                     </tbody>
                   </table>
@@ -331,18 +331,22 @@ function CustomerOrder({}) {
                     </Form.Label>
                   </div>
                 </div>
-                {pickup == 'home-delivery' ?
-                  deliveryAddress ?
+                {pickup == "home-delivery" ? (
+                  deliveryAddress ? (
                     <div className="section-border">
                       <div className="">
                         <span className="bg-coloring">Delivery Address:</span>
                         <p>
-                          <strong>{deliveryAddress.city},{deliveryAddress.address}</strong>
+                          <strong>
+                            {deliveryAddress.city},{deliveryAddress.address}
+                          </strong>
                         </p>
                       </div>
                       <div className="">
                         <span className="bg-coloring">Indication:</span>
-                        <p><strong>{deliveryAddress.indication}</strong></p>
+                        <p>
+                          <strong>{deliveryAddress.indication}</strong>
+                        </p>
                       </div>
                       <div className="">
                         <button
@@ -353,7 +357,7 @@ function CustomerOrder({}) {
                         </button>
                       </div>
                     </div>
-                    :
+                  ) : (
                     <div className="section-border">
                       <div className="d-grid gap-2 col-6 mx-auto">
                         <span className="no-address">No Address Found.</span>
@@ -366,8 +370,8 @@ function CustomerOrder({}) {
                         <br></br>
                       </div>
                     </div>
-                  : null}
-                
+                  )
+                ) : null}
               </div>
               <div className="col-md-4 block_01">
                 <h4>Member?</h4>
@@ -474,9 +478,7 @@ function CustomerOrder({}) {
             <br></br>
             <div className="input_field two_part">
               <div className="wid">
-                <Form.Label className="label-style">
-                  City Name
-                </Form.Label>
+                <Form.Label className="label-style">City Name</Form.Label>
                 <Form.Control
                   id="field-style"
                   type="text"
@@ -502,24 +504,19 @@ function CustomerOrder({}) {
             </div>
             <div className="input_field two_part">
               <div className="wid">
-                <Form.Label className="label-style">
-                  Address
-                </Form.Label>
+                <Form.Label className="label-style">Address</Form.Label>
                 <Form.Control
                   id="field-style"
                   type="text"
                   placeholder="Address"
                   onChange={(event) => {
-                    setAddress(event.target.value)
+                    setAddress(event.target.value);
                   }}
                 ></Form.Control>
               </div>
             </div>
             <div className="d-grid gap-2 col-6 mx-auto">
-              <button
-                className="btn btn-warning top-space"
-                onClick={Save}
-              >
+              <button className="btn btn-warning top-space" onClick={Save}>
                 <i className="bi bi-save-fill"></i>Save
               </button>
               <br></br>
