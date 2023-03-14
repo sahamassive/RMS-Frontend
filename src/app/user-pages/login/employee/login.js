a0f8727d3ac868c2a3943e14abeed0fee574f114
internMassiveStar <intern.massive@gmail.com>
2023-03-14 12:10:20 +0600

    commit from kalam

--- src/app/user-pages/login/employee/login.js
+++ src/app/user-pages/login/employee/login.js
@@ -28,7 +28,7 @@ function Login() {
       formData.append("device_name", "web");
 
       await axios
-        .post(`${baseUrl}/api/login-dashboard`, formData)
+        .post(`${baseUrl}/api/login`, formData)
 
         .then(({ data }) => {
           if (data.message == "Login") {
@@ -61,66 +61,66 @@ function Login() {
 
   return (
     <div className="background-log">
-        <div className="col-md-12 section-04">
-          <div className="col-md-2"></div>
-          <div className="col-md-3">
-            <img
-              className="logo"
-              src={require("../../../../assets/images/logo.png")}
-              alt=""
-            ></img>
-            <p className="company_name">
-              <span className="res">Restaurant</span> FOOD
-            </p>
-            <select
-              id="field-style-2"
-              onChange={(event) => {
-                setType(event.target.value);
-              }}
-            >
-              <option value="">Select Section</option>
-              <option value="Chef">Chef</option>
-              <option value="Waiter">Waiter</option>
-              <option value="Manager">Manager</option>
-              <option value="Super-Admin">Super Admin</option>
-              <option value="Admin">Admin</option>
-            </select>
-            <Form.Control
-              name="username"
-              type="text"
-              placeholder="Email Address or username"
-              onChange={(event) => {
-                setEmail(event.target.value);
-              }}
-            />
-            <Form.Control
-              name="password"
-              type="password"
-              placeholder="Password"
-              onChange={(event) => {
-                setPassword(event.target.value);
-              }}
-            />
-            <button
-              className="btn btn-warning space"
-              type="submit"
-              onClick={loginEmployee}
-            >
-              Sign in <i className="bi bi-box-arrow-in-right"></i>
-            </button>
-            <a href="" className="forget">
-              Forget Password?
-            </a>
-          </div>
-          <div className="col-md-5">
-            <img
-              className="right-pic"
-              src={require("../../../../assets/images/login-page.jpg")}
-              alt=""
-            ></img>
-          </div>
-          <div className="col-md-2"></div>
+      <div className="col-md-12 section-04">
+        <div className="col-md-2"></div>
+        <div className="col-md-3">
+          <img
+            className="logo"
+            src={require("../../../../assets/images/logo.png")}
+            alt=""
+          ></img>
+          <p className="company_name">
+            <span className="res">Restaurant</span> FOOD
+          </p>
+          <select
+            id="field-style-2"
+            onChange={(event) => {
+              setType(event.target.value);
+            }}
+          >
+            <option value="">Select Section</option>
+            <option value="Chef">Chef</option>
+            <option value="Waiter">Waiter</option>
+            <option value="Manager">Manager</option>
+            <option value="Super-Admin">Super Admin</option>
+            <option value="Admin">Admin</option>
+          </select>
+          <Form.Control
+            name="username"
+            type="text"
+            placeholder="Email Address or username"
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
+          <Form.Control
+            name="password"
+            type="password"
+            placeholder="Password"
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
+          <button
+            className="btn btn-warning space"
+            type="submit"
+            onClick={loginEmployee}
+          >
+            Sign in <i className="bi bi-box-arrow-in-right"></i>
+          </button>
+          <a href="" className="forget">
+            Forget Password?
+          </a>
         </div>
+        <div className="col-md-5">
+          <img
+            className="right-pic"
+            src={require("../../../../assets/images/login-page.jpg")}
+            alt=""
+          ></img>
+        </div>
+        <div className="col-md-2"></div>
+      </div>
     </div>
   );
 }
